6317776d31d7134c159098d6ffc2abfb8b21dbd4
Joshuafy <50149688+Joshuafy@users.noreply.github.com>
2021-03-07 16:31:46 -0500

    Scripts to generate slot assignments

--- pop_troopslots.py
+++ pop_troopslots.py
@@ -1,11 +1,3 @@
-"""
-Troops
-   (troop_set_slot, "trp_pendor_cavalry", 169, 81), - item measure
-   (troop_set_slot, "trp_pendor_cavalry", 170, 32), - skill measure
-   (troop_set_slot, "trp_pendor_cavalry", 171, 23), - Horse quality
-   (troop_set_slot, "trp_pendor_cavalry", 172, 1), - 1 mounted, 2 archer, 3 mountedarcher
-"""
- 
 line_num = 0
 with open('troopsource.txt') as file:
 	for line in file:
